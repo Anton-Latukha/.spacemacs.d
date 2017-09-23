928cac048616d084e1bb801c2012e59406ea6f45
Anton-Latukha <anton.latukha@gmail.com>
2017-09-23 16:04:32 +0300

    init.el: layers: crt Manual block

--- init.el
+++ init.el
@@ -49,6 +49,9 @@ values."
      ;; spell-checking
      ;; syntax-checking
      ;; version-control
+     ;; ----------------------------------------------------------------
+     ;; Manually configured block
+     ;; ----------------------------------------------------------------
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
