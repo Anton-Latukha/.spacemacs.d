7f72c70ad6fbe780eff18ac66609f065b188029a
Anton-Latukha <anton.latukha@gmail.com>
2018-07-25 14:46:45 +0300

    init.el: font: comment-in

--- init.el
+++ init.el
@@ -253,11 +253,11 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Inconsolata LGC"
-                               :size 13
-                               :weight normal
-                               :width normal
-                               :powerline-scale 1.1)
+   ;; dotspacemacs-default-font '("Inconsolata LGC"
+   ;;                             :size 13
+   ;;                            :weight normal
+   ;;                            :width normal
+   ;;                            :powerline-scale 1.1)
    ;; The leader key
    dotspacemacs-leader-key "SPC"
    ;; The key used for Emacs commands (M-x) (after pressing on the leader key).
