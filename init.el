3627e85eff7d27ff349bd01683c2b32d234d3a8a
Anton-Latukha <anton.latukha@gmail.com>
2019-02-15 14:47:22 +0200

    init: org: hide tags from the eyes

--- init.el
+++ init.el
@@ -800,7 +800,7 @@ before packages are loaded."
   (custom-theme-set-faces
    'user
    ;; Customize the Emacs theme to set all tags the same
-   `(org-tag                   ((t (:foreground "black" :weight bold :height 0.8))))
+   `(org-tag                   ((t (:foreground "gray18" :weight bold :height 0.8))))
    `(org-property-value        ((t (:foreground "black" :weight bold :height 0.8))))
    )
   )
