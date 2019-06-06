2235795e56077b79bdab591401132a6d0517b67e
Anton-Latukha <anton.latukha@gmail.com>
2019-06-06 14:40:50 +0300

    upd custom code

--- init.el
+++ init.el
@@ -1281,7 +1281,5 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-drawer ((t (:foreground "black" :weight bold :height 0.8))))
- '(org-property-value ((t (:foreground "black" :weight bold :height 0.8))) t)
- '(org-tag ((t (:foreground "gray18" :weight bold :height 0.8)))))
+ )
 )
