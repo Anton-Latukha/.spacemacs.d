60191ac25e4e7c6d6c3af235d39112e290f435c1
Anton-Latukha <anton.latukha@gmail.com>
2019-06-12 14:40:41 +0300

    upd fun name to my-org-toggle-tag-drill

--- init.el
+++ init.el
@@ -1165,7 +1165,7 @@ before packages are loaded."
     (interactive)
     (helm-org-rifle-files (append my-org-refile-additional-targets org-agenda-files))
     )
-  (defun my-toggle-tag-drill ()
+  (defun my-org-toggle-tag-drill ()
     "Toggle :drill: tag"
     (interactive)
     (org-toggle-tag "drill")
@@ -1181,7 +1181,7 @@ before packages are loaded."
   (spacemacs/declare-prefix-for-mode 'org-mode "o" "custom")
   (spacemacs/set-leader-keys-for-major-mode 'org-mode
     "oi" 'org-id-get-create
-    "od" 'my-toggle-tag-drill
+    "od" 'my-org-toggle-tag-drill
     )
 
   (add-to-list 'load-path "~/.spacemacs.d/lisp/org-protocol-capture-html")
