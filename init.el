9132d8e29ef469cc89932a3cf1fd16bfc38fadeb
Anton-Latukha <anton.latukha@gmail.com>
2019-10-03 00:01:17 +0300

    init: rm org-habit customization - it is hardcoded

--- init.el
+++ init.el
@@ -370,16 +370,6 @@ This function should only modify configuration layer settings."
           org-fontify-whole-heading-line t
           org-fontify-done-headline t
           org-fontify-quote-and-verse-blocks t
-          ;;;; 2019-06-10: NOTE: Submitted a bug report that Org-habit is hardcoded for default org-log-note-headings
-          ;; org-log-note-headings '((done        . "CLOSED %t: NOTE:")
-          ;;                         (state       . "%t: upd state %-12s (old %-12S)")
-          ;;                         (note        . "%t: NOTE:")
-          ;;                         (reschedule  . "%t: re-scheduled %S")
-          ;;                         (delschedule . "%t: unscheduled %S")
-          ;;                         (redeadline  . "%t: add deadline %S")
-          ;;                         (deldeadline . "%t: rm deadline %S")
-          ;;                         (refile      . "%t: re-filed")
-          ;;                         (clock-out   . ""))
           org-export-headline-levels 10
           org-enable-github-support t
           org-enable-bootstrap-support t
