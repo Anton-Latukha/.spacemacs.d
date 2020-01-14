df4b02ca56cb76dcc22aaec1e56a1dc5d433de74
Anton-Latukha <anton.latukha@gmail.com>
2020-01-14 18:03:09 +0200

    theme: elfeed: add face for feed

--- init.el
+++ init.el
@@ -606,6 +606,10 @@ This function should only modify configuration layer settings."
           :foreground
           "gray20"
           )
+         (elfeed-search-feed-face
+          :foreground
+          "gray16"
+          )
 
       ;; See: https://code.orgmode.org/bzg/org-mode/src/master/lisp/org-faces.el
       ;; (custom-theme-set-faces
