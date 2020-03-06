c14a00c6e35fcb6cb56db130f3c7d32a044ecf80
Anton-Latukha <anton.latukha@gmail.com>
2020-03-07 01:24:27 +0200

    dotspacemacs-line-numbers: rm for the time

--- init.el
+++ init.el
@@ -1064,11 +1064,7 @@ It should only modify the values of Spacemacs settings."
    ;;   :size-limit-kb 1000)
    ;; When used in a plist, `visual' takes precedence over `relative'.
    ;; (default nil)
-   dotspacemacs-line-numbers
-   '(
-     :relative t
-     :size-limit-kb 1000
-     )
+   dotspacemacs-line-numbers nil
 
    ;; Code folding method. Possible values are `evil' and `origami'.
    ;; (default 'evil)
