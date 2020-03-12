bf1ab95a69e2ac23ffeb4462dc59b40f632ab9b2
Anton-Latukha <anton.latukha@gmail.com>
2020-03-13 00:45:58 +0200

    layer: org: latex: rm packages-alist tikz setup
    M  init.el

--- init.el
+++ init.el
@@ -405,10 +405,6 @@ This function should only modify configuration layer settings."
       ;; org-latex-compiler "xelatex"    ;; 'xelatex' was wrongly encoding the LaTeX formulas
       ;; org-latex-inputenc-alist '(("utf8" . "utf8x"))    ;; Add Unicode char support, also extends ("mathletters" "ucs" nil)
       org-latex-listings 'minted    ;; Use Python Pygments highlighter. Needs `org-latex-pdf-process' with `-shell-escape'
-      ;; org-latex-packages-alist '(
-      ;;                            ("" "tikz" t)
-      ;;                            ("" "tikz-cd" t)
-      ;;                            )
 
       ;; Works perfectly, generates fast, shows high-class, needs Emacs to support svg display
       org-preview-latex-default-process 'dvisvgm  ;; imagemagick
