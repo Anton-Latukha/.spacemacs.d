3699fc491da430bfa3747c46c4fabe1e048caea5
Anton-Latukha <anton.latukha@gmail.com>
2020-03-17 02:24:20 +0200

    init: add co preview process LuaTex PDF -> Inkscape SVG
    M  init.el

--- init.el
+++ init.el
@@ -2468,6 +2468,17 @@ with DRILL_CARD_TYPE nil."
   ;;                           ("pdf2svg %f %O"))
   ;;              )
 
+  ;; (add-to-list 'org-preview-latex-process-alist
+  ;;              '(lua2svg :programs
+  ;;                           ("lualatex" "inkscape")
+  ;;                           :description "pdf > svg" :message "you need to install the programs: lualatex and inkscape." :image-input-type "pdf" :image-output-type "svg" :image-size-adjust
+  ;;                           (1.0 . 1.0)
+  ;;                           :latex-compiler
+  ;;                           ("lualatex --output-format pdf --shell-escape --interaction=nonstopmode --output-directory=%o %f")
+  ;;                           :image-converter
+  ;;                           ("inkscape --vacuum-defs --without-gui --export-background-opacity='0.0' --export-area-drawing=%O %f"))
+  ;;              )
+
   )
 
 
