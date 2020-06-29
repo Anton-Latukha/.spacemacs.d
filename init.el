49c7791fd361e7a27de4919fd87d3f0aa8985aef
Anton-Latukha <anton.latukha@gmail.com>
2020-06-29 22:04:01 +0300

    m upd
    M  init.el

--- init.el
+++ init.el
@@ -2606,7 +2606,7 @@ with DRILL_CARD_TYPE nil."
 
   (add-to-list 'load-path "~/.spacemacs.d/lisp/lisp/org-sync")
 
-;;;;;  Load the org-sync backends "I need"
+;;;;; Load the org-sync backends "I need"
   (mapc 'load
         '("org-sync" "org-sync-bb" "org-sync-github" "org-sync-redmine"))
 
