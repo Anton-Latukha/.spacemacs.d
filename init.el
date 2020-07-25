f7b76e292990546413087c8a9878d449b318dc3d
Anton-Latukha <anton.latukha@gmail.com>
2020-07-25 19:32:14 +0300

    M  init.el

--- init.el
+++ init.el
@@ -2634,6 +2634,25 @@ with DRILL_CARD_TYPE nil."
   (setq my-token-github-org-sync (my-get-file-char-range-as-string "/i/Personal/Technical/Files/Secure/token-rogue-github-org-sync" 0 40))
   (setq org-sync-github-auth `("Anton-Latukha" . ,my-token-github-org-sync))
 
+;; ;;;; Workaround of Tramp & git-gutter+-refresh wrong type argument
+
+;;   ;; fixes to git-gutter+ when using tramp
+;;   (with-eval-after-load 'git-gutter+
+;;     ;; https://github.com/syl20bnr/spacemacs/issues/12860#issuecomment-602084919
+;;     (defun git-gutter+-remote-default-directory (dir file)
+;;       (let* ((vec (tramp-dissect-file-name file))
+;;              (method (tramp-file-name-method vec))
+;;              (user (tramp-file-name-user vec))
+;;              (domain (tramp-file-name-domain vec))
+;;              (host (tramp-file-name-host vec))
+;;              (port (tramp-file-name-port vec)))
+;;         (tramp-make-tramp-file-name method user domain host port dir)))
+
+;;     (defun git-gutter+-remote-file-path (dir file)
+;;       (let ((file (tramp-file-name-localname (tramp-dissect-file-name file))))
+;;         (replace-regexp-in-string (concat "\\`" dir) "" file)))
+;;     )
+
 ;;;; )
   )
 
