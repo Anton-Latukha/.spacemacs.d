27365202115031962f4bee6ed16bdaf2e536306f
Anton-Latukha <anton.latukha@gmail.com>
2020-08-28 20:04:03 +0300

    init.el: enable git-gutter+ workaround
    M  init.el

--- init.el
+++ init.el
@@ -2635,24 +2635,24 @@ with DRILL_CARD_TYPE nil."
   (setq my-token-github-org-sync (my-get-file-char-range-as-string "/i/Personal/Technical/Files/Secure/token-rogue-github-org-sync" 0 40))
   (setq org-sync-github-auth `("Anton-Latukha" . ,my-token-github-org-sync))
 
-;; ;;;; Workaround of Tramp & git-gutter+-refresh wrong type argument
+;;;; Workaround of Tramp & git-gutter+-refresh wrong type argument
 
-;;   ;; fixes to git-gutter+ when using tramp
-;;   (with-eval-after-load 'git-gutter+
-;;     ;; https://github.com/syl20bnr/spacemacs/issues/12860#issuecomment-602084919
-;;     (defun git-gutter+-remote-default-directory (dir file)
-;;       (let* ((vec (tramp-dissect-file-name file))
-;;              (method (tramp-file-name-method vec))
-;;              (user (tramp-file-name-user vec))
-;;              (domain (tramp-file-name-domain vec))
-;;              (host (tramp-file-name-host vec))
-;;              (port (tramp-file-name-port vec)))
-;;         (tramp-make-tramp-file-name method user domain host port dir)))
+  ;; fixes to git-gutter+ when using tramp
+  (with-eval-after-load 'git-gutter+
+    ;; https://github.com/syl20bnr/spacemacs/issues/12860#issuecomment-602084919
+    (defun git-gutter+-remote-default-directory (dir file)
+      (let* ((vec (tramp-dissect-file-name file))
+             (method (tramp-file-name-method vec))
+             (user (tramp-file-name-user vec))
+             (domain (tramp-file-name-domain vec))
+             (host (tramp-file-name-host vec))
+             (port (tramp-file-name-port vec)))
+        (tramp-make-tramp-file-name method user domain host port dir)))
 
-;;     (defun git-gutter+-remote-file-path (dir file)
-;;       (let ((file (tramp-file-name-localname (tramp-dissect-file-name file))))
-;;         (replace-regexp-in-string (concat "\\`" dir) "" file)))
-;;     )
+    (defun git-gutter+-remote-file-path (dir file)
+      (let ((file (tramp-file-name-localname (tramp-dissect-file-name file))))
+        (replace-regexp-in-string (concat "\\`" dir) "" file)))
+    )
 
 ;; ;;;; Org-roam config ;;  2020-08-24: FIXME: Org-roam is bugged-out, enable on release
 ;;   (add-hook 'org-mode-hook #'org-roam-mode)
