6f6bd1244d487700ac13e1b8c616376e3a320ce2
Anton-Latukha <anton.latukha@gmail.com>
2020-10-14 19:12:53 +0300

    clean-up
    M  init.el

--- init.el
+++ init.el
@@ -404,32 +404,7 @@ This function should only modify configuration layer settings."
       ;; Setting hyperref in LaTeX overrides this template, and has a side effect - adds epty space to the previews
       org-latex-hyperref-template "\\hypersetup{\n pdfauthor={%a},\n pdftitle={%t},\n pdfkeywords={%k},\n pdfsubject={%d},\n pdfcreator={%c},\n pdflang={%L},\n colorlinks=true,\n linkcolor=blue,\n urlcolor=violet,\n filecolor=violet,\n bookmarksdepth=10}\n"
 
-      ;; org-babel-inline-result-wrap "%s"
-
       org-latex-compiler "lualatex"
-
-      ;; org-preview-latex-process-alist
-      ;; '(
-      ;;   (dvipng :programs
-      ;;           ("latex" "dvipng")
-      ;;           :description "dvi > png" :message "you need to install the programs: latex and dvipng." :image-input-type "dvi" :image-output-type "png" :image-size-adjust
-      ;;           (1.0 . 1.0)
-      ;;           :latex-compiler
-      ;;           ("latex -interaction nonstopmode -output-directory %o %f")
-      ;;           :image-converter
-      ;;           ("dvipng -D %D -T tight -o %O %f"))
-      ;;            :image-converter
-      ;;            ("dvisvgm %f -n -b min -c %S -o %O"))
-      ;;   (imagemagick :programs
-      ;;                ("latex" "convert")
-      ;;                :description "pdf > png" :message "you need to install the programs: latex and imagemagick." :image-input-type "pdf" :image-output-type "png" :image-size-adjust
-      ;;                (1.0 . 1.0)
-      ;;                :latex-compiler
-      ;;                ("pdflatex -interaction nonstopmode -output-directory %o %f")
-      ;;                :image-converter
-      ;;                ("convert -density %D -trim -antialias %f -quality 100 %O")
-      ;;                )
-      ;;   )
 
       org-latex-default-packages-alist
       '(
