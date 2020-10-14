ff9dc2b2f53d939ca8cd2de2218b602bfa37833f
Anton-Latukha <anton.latukha@gmail.com>
2020-10-14 19:20:19 +0300

    clean-up
    M  init.el

--- init.el
+++ init.el
@@ -2575,6 +2575,7 @@ with DRILL_CARD_TYPE nil."
     "r v" 'org-roam-show-graph
     )
 
+  (setq org-roam-directory org-directory)
   (setq org-roam-index-file (concat org-roam-directory "/index.org"))
 
 ;;;; misc
